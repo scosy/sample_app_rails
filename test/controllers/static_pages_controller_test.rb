03e613a43701a846799b6ff0b029b0dcb4afde37
scosy <amine.bentaleb064@gmail.com>
2022-05-26 09:20:44 +0200

    Motivation & Strings and Methods

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,14 +5,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
     get static_pages_home_url
-    assert_select 'title', "Home | #{@base_title}"
+    assert_select 'title', @base_title
     assert_response :success
   end
 
